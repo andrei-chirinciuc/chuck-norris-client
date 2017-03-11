0c5d8f117a38930198ea1c8a91843990550367a8
Andrei Chirinciuc <andrei.chirinciuc@gmail.com>
2017-03-11 09:22:52 +0200

    Initial commit

---
+++ src/main/java/ro/webeet/ChuckNorrisClientApplication.java
@@ -0,0 +1,12 @@
+package ro.webeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisClientApplication.class, args);
+	}
+}
